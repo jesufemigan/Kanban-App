8d88f7caedd6ec4fa67aa945d9f3eccd834d0d93
ElpidaHope <paul.oladapo@yahoo.com.com>
2022-09-14 19:42:38 +0100

    untracked gitignore files

--- server/dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv = require('dotenv').config();
-const express = require('express');
-// const app = express()
-// connectDB()
-// const PORT = process.env.PORT
-// app.listen(PORT, () => console.log(`App running on PORT ${PORT}`))
-console.log(process.env.PORT);
